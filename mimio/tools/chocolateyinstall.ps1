a4441ea998cc42c03a02f572e9921c1a5742711b
hartmch <chadhartman@gmail.com>
2018-05-04 15:25:55 -0400

    Add files via upload

---
+++ mimio/tools/chocolateyinstall.ps1
@@ -0,0 +1,23 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://s3.amazonaws.com/mimiostudio11.5/mimio-studio-11.54-en.msi'
+$fileLocation = Join-Path $toolsDir 'mimio-studio-11.54-en.msi'
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url
+  file          = $fileLocation
+
+  softwareName  = 'Mimio*'
+
+  checksum      = '73AC44C2DDC47CA7D74E6113ACB437D4AEB2838AD734E59F65DC0A4C1E8BC787'
+  checksumType  = 'sha256'
+
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ mimio/tools/chocolateyuninstall.ps1
@@ -0,0 +1,51 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'MimioStudio'  #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  fileType      = 'MSI' #only one of these: MSI or EXE (ignore MSU for now)
+  # MSI
+  silentArgs    = "/qn /norestart"
+  validExitCodes= @(0, 3010, 1605, 1614, 1641) # https://msdn.microsoft.com/en-us/library/aa376931(v=vs.85).aspx
+}
+
+$uninstalled = $false
+
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($key.Count -eq 1) {
+  $key | % { 
+    $packageArgs['file'] = "$($_.UninstallString)"
+    if ($packageArgs['fileType'] -eq 'MSI') {
+      $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
+      $packageArgs['file'] = ''
+    }
+
+    $mimiosys = Get-Process -Name "mimiosys" -ErrorAction SilentlyContinue
+    $mimiotools = Get-Process -Name "tools" -ErrorAction SilentlyContinue
+    $mimionotebook = Get-Process -Name "notebook" -ErrorAction SilentlyContinue
+    $mimiogradebook = Get-Process -Name "gradebook" -ErrorAction SilentlyContinue 
+
+    if ($mimiosys) {
+      Stop-Process -Name "mimiosys"
+    }
+    if ($mimiotools) {
+      Stop-Process -Name "tools"
+    }
+    if ($mimionotebook) {
+      Stop-Process -Name "notebook"
+    }
+    if ($mimiogradebook) {
+      Stop-Process -Name "gradebook"
+    }
+
+    Uninstall-ChocolateyPackage @packageArgs
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $($_.DisplayName)"}
+}
+
